0e879630b281cf744c87e53a2385fcbde771da65
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2021-12-09 21:52:31 -0800

    Update sudo_db.py

--- dB/sudo_db.py
+++ dB/sudo_db.py
@@ -1,13 +1,40 @@
-class SudoDB:
-    def __init__(self):
-        pass
+from .scaffold import Scaffold
 
-    def add_sudo(self, *args, **kwargs):
-        """Function for adding sudo in a chat."""
-        pass
 
-    def del_sudo(self, *args, **kwargs):
-        pass
+class SudoDB(Scaffold):
+    def get_sudos(self, chat_id: int):
+        return [
+            row[1]
+            for row in self.cur.execute(
+                "SELECT * FROM sudo_db WHERE chat_id = ?", (chat_id,)
+            )
+        ]
 
-    def get_sudos(self, *args, **kwargs):
-        pass
+    def add_sudo(self, chat_id: int, user_id: int):
+        if user_id in self.get_sudos(chat_id):
+            return "already_become_sudo"
+        self.cur.execute(
+            "INSERT INTO sudo_db VALUES (?, ?)",
+            (
+                chat_id,
+                user_id,
+            ),
+        )
+        self.conn.commit()
+        return "added_sudo"
+
+    def del_sudo(self, chat_id: int, user_id: int):
+        if user_id not in self.get_sudos(chat_id):
+            return "already_deleted_sudo"
+        self.cur.execute(
+            "DELETE FROM sudo_db WHERE chat_id = ? AND user_id = ?",
+            (
+                chat_id,
+                user_id,
+            ),
+        )
+        self.conn.commit()
+        return "deleted_sudo"
+
+
+sudo_db = SudoDB()
